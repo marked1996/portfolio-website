4c820b5712af305d26ec54e42e4c46d12d36c927
Mark Novak <novakmark9@gmail.com>
2023-08-30 15:26:25 +0200

    disable cv btn

--- src/assets/components/popup/Popup.jsx
+++ src/assets/components/popup/Popup.jsx
@@ -4,7 +4,7 @@ const Popup = () => {
   return (
     <article
       className="z-[49] min-w-max rounded-radius py-2 px-4 bg-cardBgClr border 
-    border-borderClr text-text absolute bottom-0 right-0 translate-x-[100%] -translate-y-[60%]"
+    border-borderClr text-text absolute bottom-0 right-0 translate-x-[10%] -translate-y-[60%] lg:translate-x-[100%]"
     >
       <p>Oops sorry, the link is currently disabled.</p>
     </article>
